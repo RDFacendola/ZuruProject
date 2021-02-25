a626918f844eefdb99d533e4774c344b423c2e9c
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-02-26 00:47:56 +0100

    Adding initial camera management inside CreativePawn.

--- ZuruRDF/Source/ZuruRDF/GameModes/CreativePawn.cpp
+++ ZuruRDF/Source/ZuruRDF/GameModes/CreativePawn.cpp
@@ -2,23 +2,49 @@
 // ==================================================================== //
 
 #include "CreativePawn.h"
+#include "Math/Vector.h"
 
 // ==================================================================== //
 
 ACreativePawn::ACreativePawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Root component.
+
+	Root = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pawn"));
+	Root->SetGenerateOverlapEvents(false);
+	Root->SetHiddenInGame(true);
+
+	SetRootComponent(Root);
+
+	// Pivot component.
+
+	Pivot = CreateDefaultSubobject<USceneComponent>(TEXT("Pivot"));
+	
+	Pivot->SetupAttachment(Root);
+
+	// Camera component.
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+
+	Camera->SetupAttachment(Pivot);
+
 }
 
 void ACreativePawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void ACreativePawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	// Just move the camera along the forward vector: pivot will take care of the inclination of the camera automatically.
+
+	Camera->SetRelativeLocation(FVector{ -CameraDistance, 0.0f, CameraVerticalOffset });
+	Pivot->SetRelativeRotation(FRotator{ -CameraInclination, 0.0f, 0.0f });
 }
 
 void ACreativePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
--- ZuruRDF/Source/ZuruRDF/GameModes/CreativePawn.h
+++ ZuruRDF/Source/ZuruRDF/GameModes/CreativePawn.h
@@ -5,6 +5,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Camera/CameraComponent.h"
 
 #include "CreativePawn.generated.h"
 
@@ -26,16 +27,39 @@ public:
 
 	ACreativePawn();
 
-protected:
-	
-	virtual void BeginPlay() override;
-
-public:	
-
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	
+	virtual void BeginPlay() override;
+
+private:
+
+	// A dummy mesh to locate the pawn. Hidden in game.
+	UPROPERTY(Category = Components, VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* Root{ nullptr };
+
+	// Pivot component used to raise or lower the camera relative to its target.
+	UPROPERTY(Category = Components, VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	USceneComponent* Pivot{ nullptr };
+
+	// Main camera.
+	UPROPERTY(Category = Components, VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera{ nullptr };
+
+	// Desired distance of the camera from the target, in world units.
+	UPROPERTY(Category = Configuration, EditAnywhere)
+	float CameraDistance{ 200.0f };
+
+	// Desired vertical offset of the camera relative to its target, in world units.
+	UPROPERTY(Category = Configuration, EditAnywhere)
+	float CameraVerticalOffset{ 50.0f };
+
+	// Desired camera angle, relative to its target, in degrees.
+	UPROPERTY(Category = Configuration, EditAnywhere)
+	float CameraInclination{ 45.0f };
 };
 
 // ==================================================================== //
