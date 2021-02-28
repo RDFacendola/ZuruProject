00343aa2d94bff94793761fc6de78e66878ba0e3
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-03-01 00:39:40 +0100

    Adding implementation of ZuruDatabase::FindEntity.

--- ZuruRDF/Source/ZuruRDF/Core/ZuruDatabase.cpp
+++ ZuruRDF/Source/ZuruRDF/Core/ZuruDatabase.cpp
@@ -11,6 +11,11 @@
 
 TSubclassOf<AZuruEntity> UZuruDatabase::FindEntity(const FName& InKey) const
 {
+	if (auto Record = reinterpret_cast<FZuruDatabaseEntityRecord*>(Entities->FindRowUnchecked(InKey)))
+	{
+		return Record->EntityClass;
+	}
+
 	return {};
 }
 
